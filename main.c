e39b5c298267cd8bbfac793a5f442c422252003b
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-05-08 13:22:08 +0530

    modified main

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 int main()
 {
   List_ptr list = create_list();
-  add_to_end(list,2);
+  add_to_end(list,0);
   add_to_end(list,3);
   add_to_end(list,4);
   add_to_end(list,5);
   display(list);
   printf("\n");
-  List_ptr mapped_list = filter(&is_odd,list);
-  display(mapped_list);
+  int result = reduce(&add,list,0);
+  printf("%d",result);
 }
